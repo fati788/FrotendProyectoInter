f2da8a1ae960face64be18248207e4ba4b283941
JuanAntonioSolis <jsolcor457@g.educaand.es>
2026-05-05 20:39:13 +0200

    4marcos

--- src/api/apiservice.jsx
+++ src/api/apiservice.jsx
@@ -101,8 +101,8 @@ export const sensorService = {
     decideEstadoActuador: async (actuadorId, actuadorEstado) => {
         try {
             const response = await api.post('/automatizar/actuadores', {
-                actuadorId,
-                actuadorEstado
+                id: actuadorId,
+                estado: actuadorEstado,
             });
             return response.data;
         } catch (error) {
